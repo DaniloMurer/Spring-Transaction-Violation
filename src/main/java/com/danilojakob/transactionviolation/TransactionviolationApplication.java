fc9158b9ef844216faa316f59a769127eabea649
Danilo Jakob <danilo.murer@gmail.com>
2020-09-01 10:09:03 +0200

    CHANGE: Initial Commit

---
+++ src/main/java/com/danilojakob/transactionviolation/TransactionviolationApplication.java
@@ -0,0 +1,13 @@
+package com.danilojakob.transactionviolation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionviolationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionviolationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danilojakob/transactionviolation/TransactionviolationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danilojakob.transactionviolation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionviolationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
